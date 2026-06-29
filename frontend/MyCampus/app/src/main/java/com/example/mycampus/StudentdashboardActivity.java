338e0e75b82de7dad3867be78d6bdde2cc8987de
surbhishrimali <surbhishrimali2004@gmail.com>
2026-06-29 23:35:17 +0530

    Import android.view.View in StudentdashboardActivity.java

--- frontend/MyCampus/app/src/main/java/com/example/mycampus/StudentdashboardActivity.java
+++ frontend/MyCampus/app/src/main/java/com/example/mycampus/StudentdashboardActivity.java
@@ -3,6 +3,7 @@ package com.example.mycampus;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
