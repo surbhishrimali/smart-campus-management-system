c4f8b4bdce2bc2caf186ef95ef552412b6c3c5ed
surbhishrimali <surbhishrimali2004@gmail.com>
2026-06-29 22:50:19 +0530

    Import android.view.View in admindashboardActivity.java to fix compilation issue

--- frontend/MyCampus/app/src/main/java/com/example/mycampus/admindashboardActivity.java
+++ frontend/MyCampus/app/src/main/java/com/example/mycampus/admindashboardActivity.java
@@ -2,6 +2,7 @@ package com.example.mycampus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
